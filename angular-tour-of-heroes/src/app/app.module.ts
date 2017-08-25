02d0bad4d20f2e5daab51187705c7533f83abea3
YESID-PC\yesid <yesid.montoyap@udea.edu.co>
2017-08-25 16:37:43 -0500

    Se cambio todo el contenido de la carpeta app con los nuevos archicvos para realizar el routig en la aplicacion, tambien se cambio el archivo styles.css y el archivo index.html dentro de la carpeta src.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,19 +1,29 @@
-    import { NgModule }      from '@angular/core';
-    import { BrowserModule } from '@angular/platform-browser';
-    import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-     
-    import { AppComponent }  from './app.component';
-    import { HeroDetailComponent } from './hero-detail.component';
-     
-    @NgModule({
-      imports: [
-        BrowserModule,
-        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-      ],
-      declarations: [
-        AppComponent,
-        HeroDetailComponent
-      ],
-      bootstrap: [ AppComponent ]
-    })
-    export class AppModule { }
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
