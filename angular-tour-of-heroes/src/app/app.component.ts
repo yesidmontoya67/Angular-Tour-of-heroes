5446c8a050cf6bbc25f1d170da99eb24bc085b17
YESID-PC\yesid <yesid.montoyap@udea.edu.co>
2017-08-25 14:45:48 -0500

    Se modifico el archivo app.component.ts y se agregarn dos nuevos archivos los cuales hacen el inyeccion de la clase hero

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-export class Hero {
-  id: number;
-  name: string;
-}
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'my-app',
   template: `
@@ -77,15 +65,25 @@ styles: [`
   margin-right: .8em;
   border-radius: 4px 0 0 4px;
 }
-`]
-  
+`],
+ providers: [HeroService]
 })
 export class AppComponent  { 
  title = 'Tour of Heroes';
- heroes = HEROES;
+ heroes: Hero[];
  selectedHero: Hero; 
 
- onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+ constructor(private heroService: HeroService) { }
+ 
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+ 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
